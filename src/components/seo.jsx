70dddc3731dc28ba18cbc47c99a5895acfbbcf46
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-06-06 02:18:24 +0200

    Update seo.jsx

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import PropTypes from "prop-types";
-import Script from "next/script";
+
 const SEO = ({ pageTitle }) => {
     const title = `${pageTitle} || Altlokalt - Finn bedrifter lokalt`;
     return (
@@ -24,36 +24,7 @@ const SEO = ({ pageTitle }) => {
             />
             <link rel="manifest" href="/manifest.json" />
             <link rel="assetlinks" href="/.well-known/assetlinks.json" />
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-            />
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-                }}
-            />
-         <Script
-        id="clarity-toseestuff"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", '${process.env.CLARITY_KEY}');`,
-        }}
-      />
+           
         </Head>
     );
 };
